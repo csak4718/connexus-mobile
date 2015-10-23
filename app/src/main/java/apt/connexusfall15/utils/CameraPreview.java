3ab0a4f5e5105949dbc807ff3255b803e5b0cfe6
csak4718 <csak4718@yahoo.com.tw>
2015-10-22 21:03:45 -0500

    update

--- app/src/main/java/apt/connexusfall15/utils/CameraPreview.java
+++ app/src/main/java/apt/connexusfall15/utils/CameraPreview.java
@@ -35,6 +35,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
+            mCamera.setDisplayOrientation(90);
         } catch (IOException e) {
             Log.d(TAG, "Error setting camera preview: " + e.getMessage());
         }
